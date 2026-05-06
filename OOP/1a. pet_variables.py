cd5ff4003f0f52ac4a2f7d2754939496dc967173
krishnagorakhia <krishna.gorakhia@education.nsw.gov.au>
2026-05-06 23:25:31 +0000

    Sync changes

--- OOP/1a. pet_variables.py
+++ OOP/1a. pet_variables.py
@@ -15,3 +15,19 @@ account_balance = 129.95
 #4 Prompt user for updated credit card number and save new number
 #5 Change owner name to Alex Jones
 #6 Subtract $25 from account balance
+
+name = 'Bonnie'
+animal_category = 'Cat'
+age = 4
+vaccinated = False
+ccard = input('Enter a new credit card number, please. ')
+billing_address = '17 Park Street, The Shire 2695'
+owner_name = 'Alex Jones'
+account_balance = 129.95 -25
+
+print(name,type(age))
+print(animal_category,type(animal_category))
+print(billing_address,type(billing_address))
+print(vaccinated,type(vaccinated))
+print(owner_name,type(owner_name))
+print(account_balance,type(account_balance))
